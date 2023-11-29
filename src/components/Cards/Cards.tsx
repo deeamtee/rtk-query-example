0d823b4d4e780ed8d0c95bb5eb37138441f53ca2
Сергей Константинов <sokonstantinov@sberbank.ru>
2023-11-29 17:56:53 +0300

    Исправлен query хук

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,12 +1,17 @@
-import { useGetCardsQuery } from "@services/cards/cards"
-import s from './Cards.module.css';;
+import { useFindCardsQuery } from "@services/cards/cards";
+import s from "./Cards.module.css";
 
 export const Cards = () => {
-  const { data } = useGetCardsQuery(null);
+  const { data } = useFindCardsQuery(null);
+  console.log(data);
 
   return (
     <div className={s.cardsContainer}>
-      {data?.data.map((card) => (<div key={card.id} className={s.card}>{card.name}</div>))}
+      {data.map((card) => (
+        <div key={card.id} className={s.card}>
+          {card.name}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
